79079928e664bc6ffdc6a583ae29577da49143e5
Abhishek Tiwari <abhishektiwari1x@gmail.com>
2022-09-05 13:38:24 +0530

    Added Message

--- PrimeNum.java
+++ PrimeNum.java
@@ -15,6 +15,7 @@ for(int i=2;i*i<=n && i<10;i++){
     }
 public static void main(String[] args){
 int n=50;
+//Decreasing Time Complexity by increasing Space Complexity.
 boolean[] b=new boolean[n+1];
 prime(n,b);
 }
