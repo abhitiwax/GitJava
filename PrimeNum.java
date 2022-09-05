c1df8d0f386ead5168feac2cae5fd654a89adc03
Abhishek Tiwari <abhishektiwari1x@gmail.com>
2022-09-05 13:29:03 +0530

    initial commit

---
+++ PrimeNum.java
@@ -0,0 +1,21 @@
+public class PrimeNum{
+static void prime(int n,boolean[] b){
+for(int i=2;i*i<=n && i<10;i++){
+            if(!b[i]){
+                for(int j=2*i;j<=n;j+=i){
+                    b[j]=true;
+                }
+            }
+        }
+ for(int i=2;i<=n;i++){
+            if(!b[i]){
+                System.out.println(i);
+            }
+        }
+    }
+public static void main(String[] args){
+int n=50;
+boolean[] b=new boolean[n+1];
+prime(n,b);
+}
+}
